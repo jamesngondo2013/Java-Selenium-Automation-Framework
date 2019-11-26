b7e63162280932a46c65e1986e9d48d80b8fd427
James Ngondo <jamesngondo2013@outlook.com>
2019-11-26 15:08:56 +0000

    add java classes

---
+++ selenium-project/src/test/java/com/james/training/selenium_project/AppTest.java
@@ -0,0 +1,22 @@
+package com.james.training.selenium_project;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+	@Test
+	public void openBrowser() {
+		
+		System.out.println("Chrome opening....");
+	}
+	
+	@Test
+	public void openBrowser2() {
+		
+		System.out.println("Firefox opening....");
+	}
+}
---
+++ selenium-project/src/test/java/com/james/training/selenium_project/RestAPITest.java
@@ -0,0 +1,19 @@
+package com.james.training.selenium_project;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	 
+		@Test
+		public void openBrowser() {
+			
+			System.out.println("Safari opening....");
+		}
+		
+		@Test
+		public void openBrowser2() {
+			
+			System.out.println("Other opening....");
+		}
+}
