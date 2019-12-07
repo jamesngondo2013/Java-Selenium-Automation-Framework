a4331fce08604859caabbebf5113a4c2d6ce7da5
James Ngondo <jamesngondo2013@outlook.com>
2019-12-07 18:48:45 +0000

    modified suite xml

--- selenium-project/src/test/java/com/james/training/selenium_project/basetest/BrowserDriverFactory.java
+++ selenium-project/src/test/java/com/james/training/selenium_project/basetest/BrowserDriverFactory.java
@@ -21,21 +21,21 @@ public class BrowserDriverFactory {
 		log.info("Create driver: " + browser);
 
 		switch (browser) {
-		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			driver.set(new ChromeDriver());
-			break;
-
-		case "firefox":
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-			driver.set(new FirefoxDriver());
-			break;
-
-		default:
-			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			driver.set(new ChromeDriver());
-			break;
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+				driver.set(new ChromeDriver());
+				break;
+	
+			case "firefox":
+				System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+				driver.set(new FirefoxDriver());
+				break;
+	
+			default:
+				System.out.println("Do not know how to start: " + browser + ", starting chrome.");
+				System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+				driver.set(new ChromeDriver());
+				break;
 		}
 
 		return driver.get();
