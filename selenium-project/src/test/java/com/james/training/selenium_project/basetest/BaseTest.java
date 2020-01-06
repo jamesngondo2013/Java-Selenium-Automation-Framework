8f330edf4bd2a9471a347058943c48ecd7c35606
James Ngondo <jamesngondo2013@outlook.com>
2020-01-06 17:50:34 +0000

    set browser to chrome instead of firefox

--- selenium-project/src/test/java/com/james/training/selenium_project/basetest/BaseTest.java
+++ selenium-project/src/test/java/com/james/training/selenium_project/basetest/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
 
 	@Parameters({ "browser" })
 	@BeforeMethod(alwaysRun = true)
-	public void setUp(Method method, @Optional("firefox") String browser, ITestContext ctx) {
+	public void setUp(Method method, @Optional("chrome") String browser, ITestContext ctx) {
 		String testName = ctx.getCurrentXmlTest().getName();
 		log = LogManager.getLogger(testName);
 		
